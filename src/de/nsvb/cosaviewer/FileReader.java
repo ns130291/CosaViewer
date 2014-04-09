ce6cf2e0c1ce182c1970a37c016b99e9dd458707
ns130291 <ns130291@web.de>
2014-04-09 22:58:55 +0200

    more from vandat.c01, new class FileReader for read operations

---
+++ src/de/nsvb/cosaviewer/FileReader.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 ns130291
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.nsvb.cosaviewer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author ns130291
+ */
+public class FileReader {
+
+    public static String readAttribute(RandomAccessFile file, long pos, int length) throws IOException {
+        byte[] readBuffer = new byte[length];
+        file.seek(pos);
+        file.read(readBuffer, 0, length);
+        return new String(readBuffer, Charset.forName("ISO-8859-1")).trim();
+    }
+
+    public static byte[] readBytes(RandomAccessFile file, long pos, int length) throws IOException {
+        byte[] readBuffer = new byte[length];
+        file.seek(pos);
+        file.read(readBuffer, 0, length);
+        return readBuffer;
+    }
+
+    public static byte readByte(RandomAccessFile file, long pos) throws IOException {
+        return readBytes(file, pos, 1)[0];
+    }
+
+    public static boolean[] readBooleans(RandomAccessFile file, long pos, int length) throws IOException {
+        boolean[] result = new boolean[length];
+        char[] split = readAttribute(file, pos, length).toCharArray();
+        for (int i = 0; i < split.length; i++) {
+            result[i] = (split[i] == '1');
+        }
+        return result;
+    }
+
+    public static int readIntString(RandomAccessFile file, long pos, int length) throws IOException {
+        return Integer.parseInt(readAttribute(file, pos, length));
+    }
+}
--- src/de/nsvb/cosaviewer/Veranstaltungsdaten.java
+++ src/de/nsvb/cosaviewer/Veranstaltungsdaten.java
@@ -46,10 +46,44 @@ public class Veranstaltungsdaten {
     private String stellplatzzeit;
     private String gebührZusendungErgebnisliste;
     private String aufschlagNachmeldegebühren;
+
     private boolean aufschlagProzent;
     private boolean beginnNachmeldung;
+    private boolean zeitnahmeElektronisch;
+    private boolean altersklassenPrüfung;
+    private boolean pokalwertung;
+    private boolean auswertungNachWertungsgruppen;
+    private boolean hallenveranstaltung;
+    private boolean automQualiKennzSetzen;
+    private boolean andruckKennzBSGbeiBewertNachDLVMehrkampfabz;
+    private boolean bewertSenBereichAltersklassenfaktorenMehrkampf;
+    private boolean mitWettbewerbenDMMDAMM;
+    private boolean winderfassungKleinerU16;
+    private boolean altersklassenKleinerU10;
+    private boolean gemischteWettbewerbe;
+    //private boolean bahnenSperren;
+
+    private boolean[] gesperrtFlachGerade;
+    private boolean[] gesperrtFlachRund;
+    private boolean[] gesperrtHürdenGerade;
+    private boolean[] gesperrtHürdenRund;
+
     private Typ veranstaltungsTyp;
 
+    private int anzahlUrkundenJeWettbewerb;
+    private int anzahlBahnenFlachGerade;
+    private int anzahlBahnenFlachRund;
+    private int anzahlBahnenHürdenGerade;
+    private int anzahlBahnenHürdenRund;
+    private int anzahlAusdruckeJeWettkampflisteLauf;
+    private int anzahlAusdruckeJeWettkampflisteStaffel;
+    private int anzahlAusdruckeJeWettkampflisteHochStab;
+    private int anzahlAusdruckeJeWettkampflisteTechnik;
+    private int anzahlFreieStartpositionenLauf;
+    private int anzahlFreieStartpositionenTechnik;
+    private int anzahlAusdruckeErgebnisprotokollErfassung;
+
+    //Organisationsgebühren
     private String orgGebührErwEinzel;
     private String orgGebührErwStaffel;
     private String orgGebührErwMehr;
@@ -75,60 +109,95 @@ public class Veranstaltungsdaten {
         try {
             RandomAccessFile vFile = new RandomAccessFile(file, "r");
 
-            name = readAttribute(vFile, 0x120, 90);
-            kurzName = readAttribute(vFile, 0x17a, 40);
-            veranstalter = readAttribute(vFile, 0x1a2, 50);
-            ausrichter = readAttribute(vFile, 0x1d4, 50);
-            ort = readAttribute(vFile, 0x200, 50);
-            wettkampfstätte = readAttribute(vFile, 0x249, 50);
-            veranstaltungsNummer = readAttribute(vFile, 0x238, 13);
-            
+            name = FileReader.readAttribute(vFile, 0x120, 90);
+            kurzName = FileReader.readAttribute(vFile, 0x17a, 40);
+            veranstalter = FileReader.readAttribute(vFile, 0x1a2, 50);
+            ausrichter = FileReader.readAttribute(vFile, 0x1d4, 50);
+            ort = FileReader.readAttribute(vFile, 0x200, 50);
+            wettkampfstätte = FileReader.readAttribute(vFile, 0x249, 50);
+            veranstaltungsNummer = FileReader.readAttribute(vFile, 0x238, 13);
+
             //TODO: aktiverte datumseinträge 0x2c1+4
-            datum1 = readAttribute(vFile, 0x27b, 10);
-            datum2 = readAttribute(vFile, 0x285, 10);
-            datum3 = readAttribute(vFile, 0x28f, 10);
-            datum4 = readAttribute(vFile, 0x299, 10);
-            
-            veranstaltungsSaison = readAttribute(vFile, 0xce, 4);
-            stellplatzzeit = readAttribute(vFile, 0x9, 3);
-            gebührZusendungErgebnisliste = readAttribute(vFile, 0xc, 5);
-            aufschlagNachmeldegebühren = readAttribute(vFile, 0x11, 6);
-            aufschlagProzent = readAttribute(vFile, 0x17, 1).equals("1");
-            beginnNachmeldung = readAttribute(vFile, 0x18, 1).equals("1");
-            
-            orgGebührErwEinzel = readAttribute(vFile, 0x19, 5);
-            orgGebührErwStaffel = readAttribute(vFile, 0x28, 5);
-            orgGebührErwMehr = readAttribute(vFile, 0x37, 5);
-            orgGebührErwLauf = readAttribute(vFile, 0x46, 5);
-            orgGebührErwDMM = readAttribute(vFile, 0x55, 6);
-            
-            orgGebührU20U18Einzel = readAttribute(vFile, 0x1e, 5);
-            orgGebührU20U18Staffel = readAttribute(vFile, 0x2d, 5);
-            orgGebührU20U18Mehr = readAttribute(vFile, 0x3c, 5);
-            orgGebührU20U18Lauf = readAttribute(vFile, 0x4b, 5);
-            orgGebührU20U18DMM = readAttribute(vFile, 0x5b, 6);
-            
-            orgGebührU16U8Einzel = readAttribute(vFile, 0x23, 5);
-            orgGebührU16U8Staffel = readAttribute(vFile, 0x32, 5);
-            orgGebührU16U8Mehr = readAttribute(vFile, 0x41, 5);
-            orgGebührU16U8Lauf = readAttribute(vFile, 0x50, 5);
-            orgGebührU16U8DMM = readAttribute(vFile, 0x61, 6);
-            
-            orgGebühr3_4KampfU16U8 = readAttribute(vFile, 0x1606, 5);
-            orgGebühr4_9KampfMJU16 = readAttribute(vFile, 0x160b, 5);
-            orgGebühr4_7KampfWJU16 = readAttribute(vFile, 0x1610, 5);
-            orgGebühr5_10KampfMJU20U18 = readAttribute(vFile, 0x1615, 5);
-            orgGebühr4_7KampfWJU20U18 = readAttribute(vFile, 0x161a, 5);
-            
-            veranstaltungsTyp = Typ.values()[readByte(vFile, 0x2c8)];
-            
-            
+            datum1 = FileReader.readAttribute(vFile, 0x27b, 10);
+            datum2 = FileReader.readAttribute(vFile, 0x285, 10);
+            datum3 = FileReader.readAttribute(vFile, 0x28f, 10);
+            datum4 = FileReader.readAttribute(vFile, 0x299, 10);
+
+            veranstaltungsSaison = FileReader.readAttribute(vFile, 0xce, 4);
+            stellplatzzeit = FileReader.readAttribute(vFile, 0x9, 3);
+            gebührZusendungErgebnisliste = FileReader.readAttribute(vFile, 0xc, 5);
+            aufschlagNachmeldegebühren = FileReader.readAttribute(vFile, 0x11, 6);
+            aufschlagProzent = FileReader.readAttribute(vFile, 0x17, 1).equals("1");
+            beginnNachmeldung = FileReader.readAttribute(vFile, 0x18, 1).equals("1");
+
+            orgGebührErwEinzel = FileReader.readAttribute(vFile, 0x19, 5);
+            orgGebührErwStaffel = FileReader.readAttribute(vFile, 0x28, 5);
+            orgGebührErwMehr = FileReader.readAttribute(vFile, 0x37, 5);
+            orgGebührErwLauf = FileReader.readAttribute(vFile, 0x46, 5);
+            orgGebührErwDMM = FileReader.readAttribute(vFile, 0x55, 6);
+
+            orgGebührU20U18Einzel = FileReader.readAttribute(vFile, 0x1e, 5);
+            orgGebührU20U18Staffel = FileReader.readAttribute(vFile, 0x2d, 5);
+            orgGebührU20U18Mehr = FileReader.readAttribute(vFile, 0x3c, 5);
+            orgGebührU20U18Lauf = FileReader.readAttribute(vFile, 0x4b, 5);
+            orgGebührU20U18DMM = FileReader.readAttribute(vFile, 0x5b, 6);
+
+            orgGebührU16U8Einzel = FileReader.readAttribute(vFile, 0x23, 5);
+            orgGebührU16U8Staffel = FileReader.readAttribute(vFile, 0x32, 5);
+            orgGebührU16U8Mehr = FileReader.readAttribute(vFile, 0x41, 5);
+            orgGebührU16U8Lauf = FileReader.readAttribute(vFile, 0x50, 5);
+            orgGebührU16U8DMM = FileReader.readAttribute(vFile, 0x61, 6);
+
+            orgGebühr3_4KampfU16U8 = FileReader.readAttribute(vFile, 0x1606, 5);
+            orgGebühr4_9KampfMJU16 = FileReader.readAttribute(vFile, 0x160b, 5);
+            orgGebühr4_7KampfWJU16 = FileReader.readAttribute(vFile, 0x1610, 5);
+            orgGebühr5_10KampfMJU20U18 = FileReader.readAttribute(vFile, 0x1615, 5);
+            orgGebühr4_7KampfWJU20U18 = FileReader.readAttribute(vFile, 0x161a, 5);
+
+            veranstaltungsTyp = Typ.values()[FileReader.readByte(vFile, 0x2c8)];
+
+            anzahlBahnenFlachGerade = FileReader.readIntString(vFile, 0x6a, 2);
+            anzahlBahnenFlachRund = FileReader.readIntString(vFile, 0x6c, 2);
+            anzahlBahnenHürdenGerade = FileReader.readIntString(vFile, 0x6e, 2);
+            anzahlBahnenHürdenRund = FileReader.readIntString(vFile, 0x70, 2);
+            anzahlUrkundenJeWettbewerb = FileReader.readIntString(vFile, 0x6, 3);
+            anzahlAusdruckeJeWettkampflisteLauf = FileReader.readIntString(vFile, 0x72, 2);
+            anzahlAusdruckeJeWettkampflisteStaffel = FileReader.readIntString(vFile, 0x74, 2);
+            anzahlAusdruckeJeWettkampflisteHochStab = FileReader.readIntString(vFile, 0x76, 2);
+            anzahlAusdruckeJeWettkampflisteTechnik = FileReader.readIntString(vFile, 0x78, 2);
+            anzahlFreieStartpositionenLauf = FileReader.readIntString(vFile, 0x7a, 1);
+            anzahlFreieStartpositionenTechnik = FileReader.readIntString(vFile, 0x7c, 1);
+            anzahlAusdruckeErgebnisprotokollErfassung = FileReader.readIntString(vFile, 0x82, 2);
+
+            zeitnahmeElektronisch = FileReader.readAttribute(vFile, 0x5, 1).equals("1");
+            altersklassenPrüfung = FileReader.readAttribute(vFile, 0x89, 1).equals("1");
+            pokalwertung = FileReader.readAttribute(vFile, 0x88, 1).equals("1");
+            auswertungNachWertungsgruppen = FileReader.readAttribute(vFile, 0x8d, 1).equals("1");
+            hallenveranstaltung = FileReader.readAttribute(vFile, 0x90, 1).equals("1");
+            automQualiKennzSetzen = FileReader.readAttribute(vFile, 0x8c, 1).equals("1");
+            andruckKennzBSGbeiBewertNachDLVMehrkampfabz = FileReader.readAttribute(vFile, 0x8e, 1).equals("1");
+            bewertSenBereichAltersklassenfaktorenMehrkampf = FileReader.readAttribute(vFile, 0x8f, 1).equals("1");
+            mitWettbewerbenDMMDAMM = FileReader.readAttribute(vFile, 0xc6, 1).equals("1");
+            winderfassungKleinerU16 = FileReader.readAttribute(vFile, 0x92, 1).equals("1");
+            altersklassenKleinerU10 = FileReader.readAttribute(vFile, 0x93, 1).equals("1");
+            gemischteWettbewerbe = FileReader.readAttribute(vFile, 0xc1, 1).equals("1");
+            //bahnenSperren = FileReader.readAttribute(vFile, ??????, 1).equals("1");
+
+            gesperrtFlachGerade = FileReader.readBooleans(vFile, 0x94, 10);
+            gesperrtFlachRund = FileReader.readBooleans(vFile, 0x9e, 10);
+            gesperrtHürdenGerade = FileReader.readBooleans(vFile, 0xa8, 10);
+            gesperrtHürdenRund = FileReader.readBooleans(vFile, 0xb2, 10);
+
             for (Field field : getClass().getDeclaredFields()) {
                 field.setAccessible(true);
                 String name = field.getName();
                 Object value = field.get(this);
                 System.out.printf("%s = %s%n", name, value);
             }
+            
+            /*for(boolean t:gesperrtHürdenRund){
+                System.out.print((t)?"1":"0");
+            }*/
 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Veranstaltungsdaten.class.getName()).log(Level.SEVERE, null, ex);
@@ -142,39 +211,26 @@ public class Veranstaltungsdaten {
 
     }
 
-    private String readAttribute(RandomAccessFile file, long pos, int length) throws IOException {
-        byte[] readBuffer = new byte[length];
-        file.seek(pos);
-        file.read(readBuffer, 0, length);
-        return new String(readBuffer, Charset.forName("ISO-8859-1")).trim();
-    }
-    
-    private byte readByte(RandomAccessFile file, long pos) throws IOException {
-        byte[] readBuffer = new byte[1];
-        file.seek(pos);
-        file.read(readBuffer, 0, 1);
-        return readBuffer[0];
-    }
-    
-    public enum Typ{
-        VEREINSOFFEN ("Vereinsoffen"),
-        KREISOFFEN ("Kreisoffen"),
-        BEZIRKSOFFEN ("Bezirksoffen"),
-        LANDESOFFEN ("Landesoffen"),
-        NATIONAL ("National"),
-        INTERN_SPORTFEST ("Intern. Sportfest"),
-        INTERN_EINLADUNGSSPORTFEST ("Intern. Einladungssportfest"),
-        EAA ("EAA"),
-        IAAF ("IAAF");
-        
+    public enum Typ {
+
+        VEREINSOFFEN("Vereinsoffen"),
+        KREISOFFEN("Kreisoffen"),
+        BEZIRKSOFFEN("Bezirksoffen"),
+        LANDESOFFEN("Landesoffen"),
+        NATIONAL("National"),
+        INTERN_SPORTFEST("Intern. Sportfest"),
+        INTERN_EINLADUNGSSPORTFEST("Intern. Einladungssportfest"),
+        EAA("EAA"),
+        IAAF("IAAF");
+
         private final String name;
-        
-        Typ(String name){
+
+        Typ(String name) {
             this.name = name;
-        } 
-        
+        }
+
         @Override
-        public String toString(){
+        public String toString() {
             return name;
         }
     }
