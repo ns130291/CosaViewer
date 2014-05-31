0988296649405661dd62c4d5a2c0a8f0f6e9afa6
ns130291 <ns130291@web.de>
2014-05-31 14:05:54 +0200

    Vorgaben-UI finished

--- src/de/nsvb/cosaviewer/ui/VorgabenSceneController.java
+++ src/de/nsvb/cosaviewer/ui/VorgabenSceneController.java
@@ -22,6 +22,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -33,7 +34,7 @@ import javafx.scene.layout.GridPane;
  * @author ns130291
  */
 public class VorgabenSceneController implements Initializable {
-    
+
     @FXML
     private RunningTrack runningtrackFlach;
     @FXML
@@ -93,6 +94,12 @@ public class VorgabenSceneController implements Initializable {
     @FXML
     private Label zusendungErgebnisliste;
     @FXML
+    private Label freieStartposLauf;
+    @FXML
+    private Label freieStartposTechnik;
+    @FXML
+    private Label zeitnahme;
+    @FXML
     private GridPane flach_gerade;
     @FXML
     private GridPane flach_rund;
@@ -102,60 +109,108 @@ public class VorgabenSceneController implements Initializable {
     private GridPane hürden_rund;
     @FXML
     private CheckBox nachmeldezeitraum;
-    
+    @FXML
+    private CheckBox pokalwertung;
+    @FXML
+    private CheckBox altersklassenprüfung;
+    @FXML
+    private CheckBox wertungsgruppen;
+    @FXML
+    private CheckBox hallenveranstaltung;
+    @FXML
+    private CheckBox automQualiKennzSetzen;
+    @FXML
+    private CheckBox andruckKennz;
+    @FXML
+    private CheckBox bewertSenBereich;
+    @FXML
+    private CheckBox mitWettbewerbenDMMDAMM;
+    @FXML
+    private CheckBox winderfassungKleinerU16;
+    @FXML
+    private CheckBox altersklassenKleinerU10;
+    @FXML
+    private CheckBox gemischteWettbewerbe;
+
     public void setData(Veranstaltungsdaten v) {
         runningtrackFlach.setTracks(v.getAnzahlBahnenFlachGerade(), v.getAnzahlBahnenFlachRund(), v.getGesperrtFlachGerade(), v.getGesperrtFlachRund());
         runningtrackHürden.setTracks(v.getAnzahlBahnenHürdenGerade(), v.getAnzahlBahnenHürdenRund(), v.getGesperrtHürdenGerade(), v.getGesperrtHürdenRund());
-        
+
         bahnenFlachGerade.setText(v.getAnzahlBahnenFlachGerade() + "");
         bahnenFlachRund.setText(v.getAnzahlBahnenFlachRund() + "");
         hürdenFlachGerade.setText(v.getAnzahlBahnenHürdenGerade() + "");
         hürdenFlachRund.setText(v.getAnzahlBahnenHürdenRund() + "");
-        
+
         erwEinzel.setText(v.getOrgGebührErwEinzel());
         erwStaffel.setText(v.getOrgGebührErwStaffel());
         erwMehr.setText(v.getOrgGebührErwMehr());
         erwLauf.setText(v.getOrgGebührErwLauf());
         erwDMM.setText(v.getOrgGebührErwDMM());
-        
+
         U20U18Einzel.setText(v.getOrgGebührU20U18Einzel());
         U20U18Staffel.setText(v.getOrgGebührU20U18Staffel());
         U20U18Mehr.setText(v.getOrgGebührU20U18Mehr());
         U20U18Lauf.setText(v.getOrgGebührU20U18Lauf());
         U20U18DMM.setText(v.getOrgGebührU20U18DMM());
-        
+
         U16U8Einzel.setText(v.getOrgGebührU16U8Einzel());
         U16U8Staffel.setText(v.getOrgGebührU16U8Staffel());
         U16U8Mehr.setText(v.getOrgGebührU16U8Mehr());
         U16U8Lauf.setText(v.getOrgGebührU16U8Lauf());
         U16U8DMM.setText(v.getOrgGebührU16U8DMM());
-        
+
         g3_4KampfU16U8.setText(v.getOrgGebühr3_4KampfU16U8());
         g4_9KampfMJU16.setText(v.getOrgGebühr4_9KampfMJU16());
         g4_7KampfWJU16.setText(v.getOrgGebühr4_7KampfWJU16());
         g5_10KampfMJU20U18.setText(v.getOrgGebühr5_10KampfMJU20U18());
         g4_7KampfWJU20U18.setText(v.getOrgGebühr4_7KampfWJU20U18());
-        
+
         nachmeldezeitraum.setSelected(v.isBeginnNachmeldung());
         aufschlagNachmeldung.setText(v.getAufschlagNachmeldegebühren() + ((v.isAufschlagProzent()) ? " %" : ""));
         zusendungErgebnisliste.setText(v.getGebührZusendungErgebnisliste());
+
+        freieStartposLauf.setText(v.getAnzahlFreieStartpositionenLauf() + "");
+        freieStartposTechnik.setText(v.getAnzahlFreieStartpositionenTechnik() + "");
         
+        zeitnahme.setText(v.isZeitnahmeElektronisch()?"elektronisch":"Handzeit");
+        
+        pokalwertung.setSelected(v.isPokalwertung());
+        altersklassenprüfung.setSelected(v.isAltersklassenPrüfung());
+        wertungsgruppen.setSelected(v.isAuswertungNachWertungsgruppen());
+        hallenveranstaltung.setSelected(v.isHallenveranstaltung());
+        automQualiKennzSetzen.setSelected(v.isAutomQualiKennzSetzen());
+        andruckKennz.setSelected(v.isAndruckKennzBSGbeiBewertNachDLVMehrkampfabz());
+        bewertSenBereich.setSelected(v.isBewertSenBereichAltersklassenfaktorenMehrkampf());
+        mitWettbewerbenDMMDAMM.setSelected(v.isMitWettbewerbenDMMDAMM());
+        winderfassungKleinerU16.setSelected(v.isWinderfassungKleinerU16());
+        altersklassenKleinerU10.setSelected(v.isAltersklassenKleinerU10());
+        gemischteWettbewerbe.setSelected(v.isGemischteWettbewerbe());
+
         gesperrteBahnen(flach_gerade, v.getAnzahlBahnenFlachGerade(), v.getGesperrtFlachGerade());
         gesperrteBahnen(flach_rund, v.getAnzahlBahnenFlachRund(), v.getGesperrtFlachRund());
         gesperrteBahnen(hürden_gerade, v.getAnzahlBahnenHürdenGerade(), v.getGesperrtHürdenGerade());
         gesperrteBahnen(hürden_rund, v.getAnzahlBahnenHürdenRund(), v.getGesperrtHürdenRund());
     }
-    
+
     private void gesperrteBahnen(GridPane bahnen, int anzahlBahnen, boolean[] gesperrt) {
+        boolean sperre = false;
         for (int i = 0; i < gesperrt.length && i < anzahlBahnen; i++) {
             Label num = new Label((i + 1) + "");
             bahnen.add(num, i, 0);
             GridPane.setHalignment(num, HPos.CENTER);
             if (gesperrt[i]) {
+                sperre = true;
                 Label x = new Label("x");
                 bahnen.add(x, i, 1);
                 GridPane.setHalignment(x, HPos.CENTER);
             }
+        }
+        if (!sperre) {
+            Label nichtGesperrt = new Label("keine gesperrt");
+            nichtGesperrt.setPadding(new Insets(0, 0, 0, 5));
+            //nichtGesperrt.setStyle(null);
+            bahnen.add(nichtGesperrt, 0, 1, 10, 1);
+            //GridPane.setHalignment(nichtGesperrt, HPos.CENTER);
         }
     }
 
@@ -166,5 +221,5 @@ public class VorgabenSceneController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         content.setFitToWidth(true);
     }
-    
+
 }
