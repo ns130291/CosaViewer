440fcd398e14946c9f1e958f927893ebeb860065
ns130291 <ns130291@web.de>
2014-05-31 22:42:04 +0200

    added Altersklassen

--- src/de/nsvb/cosaviewer/Altersklasse.java
+++ src/de/nsvb/cosaviewer/Altersklasse.java
@@ -22,14 +22,86 @@ package de.nsvb.cosaviewer;
  */
 public enum Altersklasse {
 
-    MÄNNER("Männer"),
-    FRAUEN("Frauen"),
+    MÄNNER("Männer", SubAltersklasse.AKTIVE, Geschlecht.MÄNNLICH),
+    FRAUEN("Frauen", SubAltersklasse.AKTIVE, Geschlecht.WEIBLICH),
+    M_U23("Junioren U23", SubAltersklasse.AKTIVE, Geschlecht.MÄNNLICH),
+    W_U23("Juniorinnen U23", SubAltersklasse.AKTIVE, Geschlecht.WEIBLICH),
+    MJ_U20("Männliche Jugend U20", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    WJ_U20("Weibliche Jugend U20", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    MJ_U18("Männliche Jugend U18", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    WJ_U18("Weibliche Jugend U18", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    MJ_U16("Männliche Jugend U16", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    WJ_U16("Weibliche Jugend U16", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    MJ_U14("Männliche Jugend U14", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    WJ_U14("Weibliche Jugend U14", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    MK_U12("Männliche Kinder U12", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    WK_U12("Weibliche Kinder U12", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    MK_U10("Männliche Kinder U10", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    WK_U10("Weibliche Kinder U10", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    MK_U08("Männliche Kinder U08", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    WK_U08("Weibliche Kinder U08", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M3("Kinder M03", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W3("Kinder W03", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M4("Kinder M04", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W4("Kinder W04", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M5("Kinder M05", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W5("Kinder W05", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M6("Kinder M06", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W6("Kinder W06", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M7("Kinder M07", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W7("Kinder W07", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M8("Kinder M08", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W8("Kinder W08", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M9("Kinder M09", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W9("Kinder W09", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M10("Kinder M10", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W10("Kinder W10", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M11("Kinder M11", SubAltersklasse.KINDER, Geschlecht.MÄNNLICH),
+    W11("Kinder W11", SubAltersklasse.KINDER, Geschlecht.WEIBLICH),
+    M12("Jugend M12", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    W12("Jugend W12", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    M13("Jugend M13", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    W13("Jugend W13", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    M14("Jugend M14", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    W14("Jugend W14", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    M15("Jugend M15", SubAltersklasse.JUGEND, Geschlecht.MÄNNLICH),
+    W15("Jugend W15", SubAltersklasse.JUGEND, Geschlecht.WEIBLICH),
+    SENIOREN_M30("Senioren M30", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W30("Seniorinnen W30", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M35("Senioren M35", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W35("Seniorinnen W35", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M40("Senioren M40", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W40("Seniorinnen W40", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M45("Senioren M45", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W45("Seniorinnen W45", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M50("Senioren M50", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W50("Seniorinnen W50", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M55("Senioren M55", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W55("Seniorinnen W55", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M60("Senioren M60", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W60("Seniorinnen W60", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M65("Senioren M65", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W65("Seniorinnen W65", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M70("Senioren M70", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W70("Seniorinnen W70", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M75("Senioren M75", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W75("Seniorinnen W75", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M80("Senioren M80", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W80("Seniorinnen W80", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M85("Senioren M85", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W85("Seniorinnen W85", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIOREN_M90("Senioren M90", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
+    SENIORINNEN_W90("Seniorinnen W90", SubAltersklasse.SENIOREN, Geschlecht.MÄNNLICH),
     ;
 
     private String name;
+    private SubAltersklasse sub;
+    private Geschlecht g;
 
-    private Altersklasse(String name) {
+    private Altersklasse(String name, SubAltersklasse sub, Geschlecht g) {
         this.name = name;
+        this.sub = sub;
+        this.g = g;
     }
 
     @Override
--- src/de/nsvb/cosaviewer/Wettbewerbe.java
+++ src/de/nsvb/cosaviewer/Wettbewerbe.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -54,7 +55,7 @@ public class Wettbewerbe {
         }
 
     };
-    
+
     public void read(File file) {
         try {
             read(new RandomFileReader(new RandomAccessFile(file, "r")));
@@ -62,8 +63,8 @@ public class Wettbewerbe {
             Logger.getLogger(Veranstaltungsdaten.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void read(byte[] file){
+
+    public void read(byte[] file) {
         read(new ByteArrayReader(file));
     }
 
@@ -75,10 +76,10 @@ public class Wettbewerbe {
 
             //int i = 0;
             while (reader.length() > pos) {
-                if (!reader.readAttribute(pos, 3).equals("")) {
+                if (!reader.readAttribute(pos, 3).equals("") && !reader.readAttribute(pos + 0x3, 28).equals("")) {
                     Wettbewerb w = new Wettbewerb();
                     w.setNummer(reader.readIntString(pos, 3));
-                    w.setName(reader.readAttribute(pos + 0x3, 28));//genaue Länge noch nicht bekannt
+                    w.setName(reader.readAttribute(pos + 0x3, 28));//TODO: genaue Länge noch nicht bekannt
                     w.setWindmessung(reader.readAttribute(pos + 0x2e, 1).equals("1"));
                     w.setOrgGebühr(reader.readAttribute(pos + 0x33, 6));
                     w.setUrkundenTyp(reader.readIntString(pos + 0x31, 1));
@@ -87,6 +88,8 @@ public class Wettbewerbe {
                     w.setTeilnehmerProStaffelMannschaft(reader.readIntString(pos + 0x2c, 1));
                     w.setStellplatzzeit(reader.readIntString(pos + 0x48, 3));
                     w.setPokalwertungsGruppe(reader.readIntString(pos + 0x2f, 2));
+
+                    w.setAltersklasse(extractAltersklasse(reader.readAttribute(pos + 0x14c, 25)));
 
                     wettbewerbe.add(w);
                 }
@@ -111,5 +114,178 @@ public class Wettbewerbe {
         } catch (IllegalAccessException ex) {
             Logger.getLogger(Veranstaltungsdaten.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private Altersklasse extractAltersklasse(String altersklasse) {
+        altersklasse = altersklasse.trim();
+        if (altersklasse.matches("Männer")) {
+            return Altersklasse.MÄNNER;
+        } else if (altersklasse.matches("Frauen")) {
+            return Altersklasse.FRAUEN;
+        } else if (altersklasse.matches("Senioren[A-Za-z0-9.ä ]*")) {
+            int alter = Integer.parseInt(altersklasse.substring(10, 12));
+            switch (alter) {
+                case 30:
+                    return Altersklasse.SENIOREN_M30;
+                case 35:
+                    return Altersklasse.SENIOREN_M35;
+                case 40:
+                    return Altersklasse.SENIOREN_M40;
+                case 45:
+                    return Altersklasse.SENIOREN_M45;
+                case 50:
+                    return Altersklasse.SENIOREN_M50;
+                case 55:
+                    return Altersklasse.SENIOREN_M55;
+                case 60:
+                    return Altersklasse.SENIOREN_M60;
+                case 65:
+                    return Altersklasse.SENIOREN_M65;
+                case 70:
+                    return Altersklasse.SENIOREN_M70;
+                case 75:
+                    return Altersklasse.SENIOREN_M75;
+                case 80:
+                    return Altersklasse.SENIOREN_M80;
+                case 85:
+                    return Altersklasse.SENIOREN_M85;
+                case 90:
+                    return Altersklasse.SENIOREN_M90;
+            }
+        } else if (altersklasse.matches("Seniorinnen[A-Za-z0-9.ä ]*")) {
+            int alter = Integer.parseInt(altersklasse.substring(13, 15));
+            switch (alter) {
+                case 30:
+                    return Altersklasse.SENIORINNEN_W30;
+                case 35:
+                    return Altersklasse.SENIORINNEN_W35;
+                case 40:
+                    return Altersklasse.SENIORINNEN_W40;
+                case 45:
+                    return Altersklasse.SENIORINNEN_W45;
+                case 50:
+                    return Altersklasse.SENIORINNEN_W50;
+                case 55:
+                    return Altersklasse.SENIORINNEN_W55;
+                case 60:
+                    return Altersklasse.SENIORINNEN_W60;
+                case 65:
+                    return Altersklasse.SENIORINNEN_W65;
+                case 70:
+                    return Altersklasse.SENIORINNEN_W70;
+                case 75:
+                    return Altersklasse.SENIORINNEN_W75;
+                case 80:
+                    return Altersklasse.SENIORINNEN_W80;
+                case 85:
+                    return Altersklasse.SENIORINNEN_W85;
+                case 90:
+                    return Altersklasse.SENIORINNEN_W90;
+            }
+        } else if (altersklasse.matches("Männliche[A-Za-z0-9 ]*")) {
+            int alter = Integer.parseInt(altersklasse.substring(18));
+            switch (alter) {
+                case 8:
+                    return Altersklasse.MK_U08;
+                case 10:
+                    return Altersklasse.MK_U10;
+                case 12:
+                    return Altersklasse.MK_U12;
+                case 14:
+                    return Altersklasse.MJ_U14;
+                case 16:
+                    return Altersklasse.MJ_U16;
+                case 18:
+                    return Altersklasse.MJ_U18;
+                case 20:
+                    return Altersklasse.MJ_U20;
+            }
+        } else if (altersklasse.matches("Weibliche[A-Za-z0-9 ]*")) {
+            int alter = Integer.parseInt(altersklasse.substring(18));
+            switch (alter) {
+                case 8:
+                    return Altersklasse.WK_U08;
+                case 10:
+                    return Altersklasse.WK_U10;
+                case 12:
+                    return Altersklasse.WK_U12;
+                case 14:
+                    return Altersklasse.WJ_U14;
+                case 16:
+                    return Altersklasse.WJ_U16;
+                case 18:
+                    return Altersklasse.WJ_U18;
+                case 20:
+                    return Altersklasse.WJ_U20;
+            }
+        } else if (altersklasse.matches("Jugend[A-Za-z0-9 ]*")) {
+            altersklasse = altersklasse.substring(6);
+            if (altersklasse.matches("M[0-9]{1,2}")) {
+                int alter = Integer.parseInt(altersklasse.substring(1));
+                switch (alter) {
+                    case 3:
+                        return Altersklasse.M3;
+                    case 4:
+                        return Altersklasse.M4;
+                    case 5:
+                        return Altersklasse.M5;
+                    case 6:
+                        return Altersklasse.M6;
+                    case 7:
+                        return Altersklasse.M7;
+                    case 8:
+                        return Altersklasse.M8;
+                    case 9:
+                        return Altersklasse.M9;
+                    case 10:
+                        return Altersklasse.M10;
+                    case 11:
+                        return Altersklasse.M11;
+                    case 12:
+                        return Altersklasse.M12;
+                    case 13:
+                        return Altersklasse.M13;
+                    case 14:
+                        return Altersklasse.M14;
+                    case 15:
+                        return Altersklasse.M15;
+                }
+            } else if (altersklasse.matches("W[0-9]{1,2}")) {
+                int alter = Integer.parseInt(altersklasse.substring(1));
+                switch (alter) {
+                    case 3:
+                        return Altersklasse.W3;
+                    case 4:
+                        return Altersklasse.W4;
+                    case 5:
+                        return Altersklasse.W5;
+                    case 6:
+                        return Altersklasse.W6;
+                    case 7:
+                        return Altersklasse.W7;
+                    case 8:
+                        return Altersklasse.W8;
+                    case 9:
+                        return Altersklasse.W9;
+                    case 10:
+                        return Altersklasse.W10;
+                    case 11:
+                        return Altersklasse.W11;
+                    case 12:
+                        return Altersklasse.W12;
+                    case 13:
+                        return Altersklasse.W13;
+                    case 14:
+                        return Altersklasse.W14;
+                    case 15:
+                        return Altersklasse.W15;
+                }
+            }
+        } else if (altersklasse.matches("Junioren U23")) {
+            return Altersklasse.M_U23;
+        } else if (altersklasse.matches("Juniorinnen U23")) {
+            return Altersklasse.W_U23;
+        }
+        return null;
     }
 }
